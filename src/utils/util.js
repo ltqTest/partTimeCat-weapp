9eb899f01d04daf0199f027b389ff3f8eef67e6e
Dumpling <841845827@qq.com>
2019-02-25 14:43:24 +0800

    topicList component

--- src/utils/util.js
+++ src/utils/util.js
@@ -1,6 +1,12 @@
 import moment from 'moment'
 import 'moment/locale/zh-cn'
 
+/**
+ * 将国际时间格式转为文字格式
+ * @param date
+ * @param format
+ * @returns {string}
+ */
 const diffForHumans = (date, format = 'YYYYMMDD H:mm:ss') => {
   moment.locale('zh-cn')
   return moment(date, format).fromNow()
